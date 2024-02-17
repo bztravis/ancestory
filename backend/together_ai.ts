557d31ea0227250307074e2f44acc000a6d06f07
adviti <adviti@umich.edu>
2024-02-17 15:58:47 -0800

    Schema for metadata on story

--- backend/together_ai.ts
+++ backend/together_ai.ts
@@ -16,3 +16,17 @@ const client = Instructor({
   client: togetherai,
   mode: "JSON_SCHEMA",
 });
+
+// Schema for information retrieval from transcript
+const StorySchema = z.object({
+  title: z.string().describe("A short descriptive title of the story."),
+  summary: z
+    .string()
+    .describe("A short summary of the story narrated")
+    .max(1000),
+  year: z.string().describe("The year the story took place").max(500),
+  place: z.string().describe("The place the story is situated in"),
+  characters: z
+    .array(z.string())
+    .describe("A list of unique names of characters mentioned in the story"),
+});

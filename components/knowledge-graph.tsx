efba3b0e1d51525582d71aabd4b40fff17d567ef
“23jmo” <2023johnathanmo@gmail.com>
2024-02-17 19:54:43 -0800

    starting graph

--- components/knowledge-graph.tsx
+++ components/knowledge-graph.tsx
@@ -13,20 +13,20 @@ const nodes = [
       }
     ];
     
-    const edges = [
-      {
-        source: '1',
-        target: '2',
-        id: '1-2',
-        label: '1-2'
-      },
-      {
-        source: '2',
-        target: '1',
-        id: '2-1',
-        label: '2-1'
-      }
-    ];
+const edges = [
+    {
+    source: '1',
+    target: '2',
+    id: '1-2',
+    label: '1-2'
+    },
+    {
+    source: '2',
+    target: '1',
+    id: '2-1',
+    label: '2-1'
+    }
+];
 
 export const MyDiagram = ( ) => (
     

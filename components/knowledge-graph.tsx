0a9f1906226e3c55bd3553e999e19b3c66c343ee
“23jmo” <2023johnathanmo@gmail.com>
2024-02-17 19:52:29 -0800

    starting graph

---
+++ components/knowledge-graph.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { GraphCanvas } from 'reagraph';
+
+
+const nodes = [
+    {
+        id: '1',
+        label: '1'
+      },
+      {
+        id: '2',
+        label: '2'
+      }
+    ];
+    
+    const edges = [
+      {
+        source: '1',
+        target: '2',
+        id: '1-2',
+        label: '1-2'
+      },
+      {
+        source: '2',
+        target: '1',
+        id: '2-1',
+        label: '2-1'
+      }
+    ];
+
+export const MyDiagram = ( ) => (
+    
+  <GraphCanvas
+    nodes={nodes}
+    edges={edges}
+  />
+);

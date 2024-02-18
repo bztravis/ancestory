9597695855c1c584503fc5fedb6810fb1835e0f7
adviti <adviti@umich.edu>
2024-02-17 16:02:51 -0800

    First draft of asynchronous function

--- backend/together_ai.ts
+++ backend/together_ai.ts
@@ -30,3 +30,40 @@ const StorySchema = z.object({
     .array(z.string())
     .describe("A list of unique names of characters mentioned in the story"),
 });
+
+// Make the request to the AI
+async function extractInformation(transcript) {
+  try {
+    const response = await client.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content:
+            "The following is a transcript of an audio recording between an individual and their older family member. This audio recording is a story being shared by the older family member. Extract a title, a summary, and a list of characters (names of people) mentioned in the story. Return it as a JSON object in this format: {title: string, summary: string, characters: [string, string, string, ...]}  ",
+        },
+        {
+          role: "user",
+          content: transcript,
+        },
+      ],
+      model: "meta-llama/Llama-2-13b-chat-hf",
+      response_model: {
+        schema: StorySchema,
+        name: "AnalyzeStory",
+        max_tokens: 1000,
+        temperature: 0.8,
+      },
+    });
+    // print the response
+    console.log(JSON.stringify(response, null, 2));
+    // return it
+    return response;
+  } catch (error) {
+    console.log("Failed to extract information from the transcript:", error);
+    throw error;
+  }
+}
+
+// transcript dummy data
+const transcript =
+  "Back in Mumbai, I dreamed about going to the University of Michigan. Over there, I met JMo and Brian. Brian was cool.";
